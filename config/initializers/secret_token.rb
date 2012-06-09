53f2d332ea5ace764248844ddbcb189ba628e058
Justin Patera <serialhex@gmail.com>
2012-06-09 14:34:50 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DuckingOctoWight::Application.config.secret_token = 'ab923bc90f798c33cc98039b46ca4f0cfad406ec2e742c283ff7e8c2e9336b3d8afbb03bd5208beb608439569e0792ab8967e3579ba9ce2d698ca23c6604e1e5'
